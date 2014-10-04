18ee89c6977a891b7160289c6b0ea3534b74d0b1
Brett C. Dudo <brett.dudo@gmail.com>
2014-10-03 22:29:58 -0700

    ummm...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
+  # Set to :info typically
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
